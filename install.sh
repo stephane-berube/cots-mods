6d66cdec9d5fc935961d81984928c2978107e4f4
Stéphane Bérubé <stephane.berube@canada.ca>
2020-05-14 13:51:27 -0400

    Bitbucket updates

--- install.sh
+++ install.sh
@@ -1,21 +1,22 @@
 #!/bin/bash -xe
 
 if [[ $# -lt 3 ]] ; then
-    echo "Usage: setup.sh <installerUrl> <dataVolume> <appVolume>"
+    echo "Usage: $0 <installerUrl> <dataVolume> <appVolume> [BitbucketUrl]"
     echo ""
-    echo "Example: setup.sh 'https://example.org/installer.bin' /dev/sda /dev/sdb dev"
+    echo "Example: $0 'https://example.org/installer.bin' /dev/sda /dev/sdb dev"
     exit 1
 fi
 
 BitbucketInstallerUrl=$1
 EC2DataVolumeMount=$2
 EC2AppVolumeMount=$3
+BitbucketUrl=$4
 
 # Create directories
 mkdir -p /opt/atlassian/bitbucket /var/atlassian/application-data/bitbucket
 
 # Wait until the volume shows up
-while [ ! -e ${EC2DataVolumeMount} ]; do echo Waiting for EBS Data volume to attach; sleep 5; done
+while [ ! -e "${EC2DataVolumeMount}" ]; do echo Waiting for EBS Data volume to attach; sleep 5; done
 
 # Create filesystem
 mkfs -t xfs "${EC2DataVolumeMount}"
@@ -24,16 +25,19 @@ mkfs -t xfs "${EC2DataVolumeMount}"
 echo "${EC2DataVolumeMount}    /var/atlassian/application-data/bitbucket xfs    defaults,noatime,nofail    0    2" >> /etc/fstab
 
 # Wait until the volume shows up
-while [ ! -e ${EC2AppVolumeMount} ]; do echo Waiting for EBS App volume to attach; sleep 5; done
+while [ ! -e "${EC2AppVolumeMount}" ]; do echo Waiting for EBS App volume to attach; sleep 5; done
 
 # Create filesystem
 mkfs -t xfs "${EC2AppVolumeMount}"
 
 # Add an entry to fstab to mount volume during boot
-echo "${EC2AppVolumeMount}     /opt/atlassian/bitbucket                xfs    defaults,noatime,nofail    0    2" >> /etc/fstab
+echo "${EC2AppVolumeMount}     /opt/atlassian/                xfs    defaults,noatime,nofail    0    2" >> /etc/fstab
 
 # Mount the volumes on current boot
 mount -a
+
+# Install deps
+yum install -y fontconfig git java-1.8.0-openjdk
 
 # Get and run Bitbucket installer
 wget "${BitbucketInstallerUrl}" -O /tmp/installer.bin
@@ -50,11 +54,16 @@ systemctl disable atlbitbucket
 mv /tmp/cots-mods/atlbitbucket.service /etc/systemd/system/atlbitbucket.service
 
 # Bitbucket uses its version number as a directory name when
-# it installs. Figure out what the directory name is and add
-# a 'install-directory' symlink pointing to it. The symlink is
-# used by the systemd unit file (atlbitbucket.service)
-dir=$(find * -maxdepth 0 -type d | sort -r | head -n 1)
-ln -s "${!dir}" install-directory
+# it installs. Figure out what the directory name is rename it to 'bitbucket'
+# This folder is referenced by the systemd unit file (atlbitbucket.service)
+dir=$(find ./* -maxdepth 0 -type d | sort -r | head -n 1)
+mv "${dir}" bitbucket
+
+# If a url was provided, add it to the config file
+if [ -n "${BitbucketUrl}" ]
+then
+  echo "server.proxy-name=${BitbucketUrl}" >> /var/atlassian/application-data/bitbucket/shared/bitbucket.properties
+fi
 
 # Refresh systemd daemons since we've added a new unit file
 # start Bitbucket and enable startup at boot-time
